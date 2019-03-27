cc2fa1d44d05dff8af923cc86710c623f221959d
Khoa Tran <khoa.d.tran97@gmail.com>
2019-03-27 16:19:43 -0700

    Initial Commit, converted .wav file to text and translated to Chinese

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # JusTalk
+
+### Wenhao Tan, Amrit Thapa, Khoa Tran, Long Vu
+1. Abstract: https://docs.google.com/presentation/d/1231-SMrZi-ZwCWly-Zq6qjbRMwKIVmCrPr9n3Y4eBB4/edit?usp=sharing
+2. Mid-Semester Update: https://docs.google.com/presentation/d/1MJ8uhqp4fPv2BBnxLeWKV0CXuA7NlqNdYIj64fqnrK4/edit?usp=sharing
+
+<br/>
+
+(Prereq)Install the following:
+-----------------
+<br/>
+```
+python 2.7
+pip
+virtualenv
+Google Speech-to-Text client library: pip install --upgrade google-cloud-speech
+Google Translate client library: pip install --upgrade google-cloud-translate
+Google Text-to-Speech client library: pip install --upgrade google-cloud-texttospeech (not yet implemented)
+```
+<br/>
+<br/>
+
+Running the app
+-----------------
+To verify service account key
+<br/>
+For Mac
+`export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"`
+For Windows with PowerShell
+`$env:GOOGLE_APPLICATION_CREDENTIALS="[PATH]]"`
+For Windows with Command Prompt
+`set GOOGLE_APPLICATION_CREDENTIALS=[PATH]`
+i.e: PATH = C:\Users\username\Downloads\[FILE_NAME].json
+
+<br/>
+<br/>
---
+++ main.py
@@ -0,0 +1,48 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+# Imports the Google Cloud client library
+from google.cloud import translate
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+file_name = os.path.join('/Users','khoatran','Desktop',
+			'Repositories','JusTalk','JusTalkENV','thisisatest.wav')
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=8000,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
+print("Print out result", result)
+
+
+# Instantiates a client
+translate_client = translate.Client()
+
+# The target language
+target = 'zh-CN'
+
+# Translates some text into Russian
+translation = translate_client.translate(
+    result.alternatives[0].transcript,
+    target_language=target)
+
+print(u'Text: {}'.format(result))
+print(u'Translation: {}'.format(translation['translatedText']))
